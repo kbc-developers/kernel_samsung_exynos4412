9debf75d37c9036ac9ec4bcbeace065948214f98
sakuramilk <c.sakuramilk@gmail.com>
2013-03-22 22:59:30 +0900

    fs: fix cmdline_proc_write arg type

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -11,31 +11,31 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int proc_write( struct file* filp, const char* buffer, unsigned long count, void* data )
-{
-	char str[1024];
-	printk( KERN_INFO "[proc/cmdline.c]buffer:\ %s: \n", buffer);
-	if ( copy_from_user( str, buffer, count ) ) {
-	   printk( KERN_INFO "[proc/cmdline.c] : copy_from_user failed\n" );
-	   return -EFAULT;
-	}
-	printk( KERN_INFO "[proc/cmdline.c]str; %s: \n", str);
-	strcpy (saved_command_line, str);
-	
-	return count;
-}
-
 static int cmdline_proc_open(struct inode *inode, struct file *file)
 {
 	return single_open(file, cmdline_proc_show, NULL);
 }
 
+static int cmdline_proc_write(struct file *file, const char __user *buf,
+				size_t len, loff_t *ppos)
+{
+	char str[1000];
+	if (copy_from_user(str, buf, len)) {
+	   printk( KERN_INFO "[cmdline] copy_from_user failed.\n");
+	   return -EFAULT;
+	}
+	str[len] = '\0';
+	strcpy(saved_command_line, str);
+
+	return len;
+}
+
 static const struct file_operations cmdline_proc_fops = {
 	.open		= cmdline_proc_open,
 	.read		= seq_read,
+	.write		= cmdline_proc_write,
 	.llseek		= seq_lseek,
 	.release	= single_release,
-	.write 		= proc_write,
 };
 
 static int __init proc_cmdline_init(void)
