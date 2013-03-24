f1c59d0398d14f6863d646b6b84757199ec18b1f
sakuramilk <c.sakuramilk@gmail.com>
2013-03-24 22:22:11 +0900

    Linux kernel 3.0.41

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -24,6 +24,7 @@
 #include <linux/kthread.h>
 #include <linux/mutex.h>
 #include <linux/freezer.h>
+#include <linux/random.h>
 
 #include <asm/uaccess.h>
 #include <asm/byteorder.h>
@@ -1925,6 +1926,15 @@ int usb_new_device(struct usb_device *udev)
 #ifdef CONFIG_SAMSUNG_SMARTDOCK
 	call_battery_notify(udev, 1);
 #endif
+
+	if (udev->serial)
+		add_device_randomness(udev->serial, strlen(udev->serial));
+	if (udev->product)
+		add_device_randomness(udev->product, strlen(udev->product));
+	if (udev->manufacturer)
+		add_device_randomness(udev->manufacturer,
+				      strlen(udev->manufacturer));
+
 	device_enable_async_suspend(&udev->dev);
 	/* Register the device.  The device driver is responsible
 	 * for configuring the device and invoking the add-device
