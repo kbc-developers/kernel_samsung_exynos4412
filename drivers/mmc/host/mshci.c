78d21772de514d1f23231e6e581c65f8e40ab8e3
sakuramilk <c.sakuramilk@gmail.com>
2013-06-02 22:55:46 +0900

    mach-u1: disable MMC_CAP_ERASE

--- drivers/mmc/host/mshci.c
+++ drivers/mmc/host/mshci.c
@@ -2045,7 +2045,17 @@ int mshci_add_host(struct mshci_host *host)
 	mmc->ops = &mshci_ops;
 	mmc->f_min = 400000;
 	mmc->f_max = host->max_clk;
+#if defined(CONFIG_MACH_U1)
+	/*
+	 * removed MMC_CAP_ERASE to avoid killing faulty emmc chips
+	 *
+	 * mmc->caps |= MMC_CAP_SDIO_IRQ | MMC_CAP_ERASE;
+	 * 
+	 */
+	mmc->caps |= MMC_CAP_SDIO_IRQ;
+#else
 	mmc->caps |= MMC_CAP_SDIO_IRQ | MMC_CAP_ERASE;
+#endif
 
 	mmc->caps |= MMC_CAP_4_BIT_DATA;
 
